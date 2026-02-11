6bb90998168681df05edb2481ea82f3189c1ef44
karthik-49 <karthikkumbar70@gmail.com>
2026-02-11 04:10:13 -0500

    dump function

--- networking/errchk.h
+++ networking/errchk.h
@@ -17,3 +17,22 @@ void *ec_malloc(unsigned int size) {
         fatal("in ec_malloc() on memory allocation");
     return ptr;
 }
+
+void dump(const unsigned char *data_buffer, const unsigned int length) {
+    unsigned char byte;
+    unsigned int i,j;
+    for(i = 0; i<length; i++) {
+        byte = data_buffer[i];
+        printf("%02x ", data_buffer[i]);
+        if(((i%16)==15) || (i ==length-1)) {
+            for(j=0; j<15-(i%16); j++) {
+                byte = data_buffer[j];
+                if((byte > 31) && (byte < 127))
+                    printf("%c", byte);
+                else   
+                    printf(".");
+            }
+            printf("\n");
+        }
+    }
+}
